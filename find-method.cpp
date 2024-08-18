d4f2e3a97d0d4768008a0ce15324ca009a761f9d
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-18 13:49:58 +0200

    Add files via upload

---
+++ find-method.cpp
@@ -0,0 +1,15 @@
+// FIND METHOD CODE
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
+
+    std::cout << name.find('P');
+
+    return 0;
+}
